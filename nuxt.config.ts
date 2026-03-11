29a7b4921d021e9fcd44b0760614f6828ba5dc89
Johnny <johnny.ngare@strathmore.edu>
2026-03-11 22:55:55 +0300

    chore: configure nuxt runtimeConfig for Vercel deployment

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,14 +1,18 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
+// nuxt.config.ts
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
 
-  modules: [
-    '@nuxt/ui',
-    '@nuxtjs/supabase',
-  ],
+  modules: ['@nuxt/ui', '@nuxtjs/supabase'],
 
   css: ['~/assets/css/main.css'],
+
+  runtimeConfig: {
+    public: {
+      supabaseUrl: process.env.SUPABASE_URL,
+      supabaseKey: process.env.SUPABASE_KEY,
+    },
+  },
 
   supabase: {
     redirectOptions: {
@@ -16,6 +20,5 @@ export default defineNuxtConfig({
       callback: '/confirm',
       exclude: ['/', '/login', '/signup'],
     },
-    types: '~/types/database.types.ts',
   },
 })
